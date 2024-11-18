c74de1a9e955c80edc6b857c3837021a1dbd3b50
Aeon Levy <158340181+aeonlevy@users.noreply.github.com>
2024-11-17 23:48:00 -0500

    Finished Scrapping Website

--- Scrapping_Homework.R
+++ Scrapping_Homework.R
@@ -10,17 +10,21 @@ au_staff <- "https://www.american.edu/cas/mathstat/faculty/"
 html <- read_html(au_staff)
 html 
 
-staff_names <-html%>%
-  html_nodes(".profile-name")%>%
-  html_text(trim = TRUE) 
+staff_names <-html|> 
+  html_elements(".profile-name")|> 
+  html_text2() 
+staff_names
 
-staff_email <- html %>%
-  html_nodes(".profile-email") %>% 
-  html_text(trim = TRUE) 
 
-staff_phone <- html %>%
-  html_nodes(".profile-phone") %>% 
-  html_text(trim = TRUE) 
+staff_email <- html|> 
+  html_elements(".profile-email")|> 
+  html_text2() 
+staff_email
+
+staff_phone <- html|> 
+  html_elements(".profile-phone")|> 
+  html_text2() 
+staff_phone
 
 ###um idk if this is what im supposed to do but im just gonna make a data frame
 
